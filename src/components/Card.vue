6e849d5a992fd8bb6acef61875d8a890758b3d0f
ali-d-coded <aliallu3xa@gmail.com>
2023-11-08 00:56:38 +0530

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const { quiz } = defineProps(['quiz'])
+</script>
+<template>
+  <div class="bg-slate-200 w-fit max-w-[300px] rounded-md">
+    <img :src="quiz.img" alt="" class="h-[200px] w-[200px] rounded-t-md" />
+    <div class="my-2 p-1">
+      <h2 class="font-bold">{{ quiz.name }}</h2>
+      <p>{{ quiz.questions.length }} questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="underline text-green-700 font-extrabold text-4xl skew-x-12 bg-red-400 w-fit mx-auto">
+      Hello flskfhj
+    </h1>
+  </main>
+</template>
